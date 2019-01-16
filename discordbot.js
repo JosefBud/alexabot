f8f24833eb24f37f0b25b0ea2be03b1c9016f9c4
JosefBud <josefbud@gmail.com>
2019-01-15 22:27:14 -0500

    c

--- discordbot.js
+++ discordbot.js
@@ -32,7 +32,7 @@ client.on('ready', () => {
 client.on('message', message => {
     let msgContent = message.content.toLowerCase().replace(/[,!'.]/gi,"");
     let everyoneArray = message.guild.members.array();
-    let randomMember = everyoneArray[Math.floor(Math.random()) * everyoneArray.length];
+    let randomMember = everyoneArray[Math.floor(Math.random()) * everyoneArray.length].charAt(3);
     // function for playing a song, all three of the function arguments are strings
     function playSong(title,imageUrl,youtubeUrl) {
         message.channel.send(embed.setAuthor(`${title}, ${message.author.username}`).setImage(imageUrl));
@@ -49,7 +49,7 @@ client.on('message', message => {
         // command for testing things
         if (msgContent.includes(`alexa test`.toLowerCase())) {
             console.log("test 1: " + message.author.lastMessage.channel.id);
-            console.log("test 2: " + message.guild.members.array());
+            console.log("test 2: " + everyoneArray);
             message.channel.send("test 2: " + randomMember);
         }
         //Alexa, play despacito command

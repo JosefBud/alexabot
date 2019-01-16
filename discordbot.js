6a2ab428b6daa56bc142ee7f7bbe1a2a2c88e7af
JosefBud <josefbud@gmail.com>
2019-01-15 21:52:56 -0500

    commit

--- discordbot.js
+++ discordbot.js
@@ -47,7 +47,7 @@ client.on('message', message => {
     if (!message.author.bot) {
         // command for testing things
         if (msgContent.includes(`alexa test`.toLowerCase())) {
-            console.log(message.author.lastMessage.Message);
+            console.log(message.author.lastMessage);
         }
         //Alexa, play despacito command
        if (msgContent.includes(`alexa play despacito`.toLowerCase())) {

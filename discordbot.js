8ca9525ae7cc34b4603ed049899e7696aadcc188
JosefBud <josefbud@gmail.com>
2019-01-18 15:30:09 -0500

    commit

--- discordbot.js
+++ discordbot.js
@@ -5,33 +5,6 @@ const user = new Discord.Message();
 const SQLite = require("better-sqlite3");
 const sql = new SQLite('./scores.sqlite');
 const Commands = require('./commands.js');
-/*
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host     : config.hostname,
-  port     : config.port,
-  user     : config.user,
-  password : config.password,
-  database : config.database,
-  charset : 'utf8mb4'
-});
-*/
-
-const embed = new Discord.RichEmbed()
-//.setTitle("This is your title, it can hold 256 characters")*/
-//.setAuthor(`Let's get jiggy with it, ${user.author}`/*, "https://i.imgur.com/lm8s41J.png"*/)
-//.setColor(0x00AE86)
-//.setDescription("This is the main body of text, it can hold 2048 characters.")
-//.setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
-//.setImage("https://media.giphy.com/media/kLM9I1g8jsiAM/giphy.gif")
-//.setThumbnail("https://media.giphy.com/media/kLM9I1g8jsiAM/giphy.gif")
-//.setTimestamp()
-//.setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
-//.addField("This is a field title, it can hold 256 characters",
-//  "This is a field value, it can hold 1024 characters.")
-//.addField("Inline Field", "They can also be inline.", true)
-//.addBlankField(true)
-//.addField("Inline Field 3", "You can have a maximum of 25 fields.", true);*/
 var thatsSoSad = false;
 
 client.on('ready', () => {

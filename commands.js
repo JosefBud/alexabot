70a597a559df56a94d127869123ed59eb75cf3ec
JosefBud <josefbud@gmail.com>
2019-01-22 10:50:21 -0500

    commits

--- commands.js
+++ commands.js
@@ -66,14 +66,14 @@ const Commands = {
             const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 5000 });
                 //console.log(collector)
                 collector.on('collect', message => {
-                    if (message.content.includes("yes") || message.content.includes("yeah") || message.content.includes("ya")) {
+                    if (message.content.toLowerCase().includes("yes") || message.content.toLowerCase().includes("yeah") || message.content.toLowerCase().includes("ya") || message.content.toLowerCase().includes("sure")) {
                         if (typeof message.member.voiceChannel !== 'undefined') {
                             Commands.play(message,"alexa play despacito");
                         }
                         else {
                             message.reply(`get in a voice channel, ya bonehead`);
                         }
-                    } else if (message.content.includes("no") || message.content.includes("nah") || message.content.includes("nope")) {
+                    } else if (message.content.toLowerCase().includes("no") || message.content.toLowerCase().includes("nah") || message.content.toLowerCase().includes("nope")) {
                         message.channel.send("Okie dokie. Hope you feel better.");
                     }
                 })

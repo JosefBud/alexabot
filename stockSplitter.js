15a12385569e9faa33d97eeddd95c20297d85b05
JosefBud <josefbud@gmail.com>
2019-07-11 20:05:53 -0400

    Automatic stock splitter

---
+++ stockSplitter.js
@@ -0,0 +1,55 @@
+const config = require('./config.json');
+const axios = require('axios');
+const SQLite = require("better-sqlite3");
+const portfolios = new SQLite('./db/portfolios.sqlite');
+
+setInterval(() => {
+  const newDate = new Date();
+  const dateOptions = {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit'
+  };
+  const dateString = newDate.toLocaleDateString('en-US', dateOptions);
+  const dateArray = dateString.split("/");
+  const finalDate = dateArray[2] + "-" + dateArray[0] + "-" + dateArray[1];
+
+  const symbolList = portfolios.prepare("SELECT symbol FROM portfolios GROUP BY symbol").all();
+
+  let i = 0;
+  async function apiLimiter() {
+    setTimeout(() => {
+      axios.get('https://www.alphavantage.co/query', {
+          params: {
+            function: 'TIME_SERIES_DAILY_ADJUSTED',
+            symbol: symbolList[i].symbol,
+            apikey: config.alphaVantageKey
+          }
+        })
+        .then(response => {
+          const splitMultiplier = parseFloat(response.data['Time Series (Daily)'][finalDate]['8. split coefficient']);
+          if (splitMultiplier != 1) {
+            const userList = portfolios.prepare("SELECT * FROM portfolios WHERE symbol = ?").all(symbolList[i].symbol);
+            userList.forEach((user) => {
+              user.qty = user.qty * splitMultiplier;
+              if (user.splitDate != finalDate) {
+                portfolios.prepare("UPDATE portfolios SET qty = ? AND splitDate = ? WHERE userId = ?").run(user.qty, finalDate, user.userId);
+                console.log(`Stock split applied to ${user.userId} for ${user.symbol} at a multiplier of ${splitMultiplier}`);
+              }
+            })
+          }
+        })
+        .catch(error => {
+          console.log(error);
+        })
+
+      i++;
+
+      if (i < symbolList.length) {
+        apiLimiter();
+      }
+    }, 15000)
+  }
+
+  apiLimiter();
+}, 86400000)

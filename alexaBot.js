6b48eca87efcbd92e0d67012c4f303a5a25eb963
JosefBud <josefbud@gmail.com>
2019-10-17 12:26:41 -0400

    Sharding fixes

--- alexaBot.js
+++ alexaBot.js
@@ -2,5 +2,5 @@ const { ShardingManager } = require('discord.js');
 const config = require('./config.json');
 const manager = new ShardingManager('./discordbot.js', { token: config.token });
 
-manager.spawn();
+manager.spawn(3, 10000);
 manager.on('launch', shard => console.log(`Launched shard ${shard.id}`));
